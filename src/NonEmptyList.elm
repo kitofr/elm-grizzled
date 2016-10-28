d1d88563928df060ac40bd5199d6d1231aa63475
Kristoffer Roupé <kitofr@gmail.com>
2016-10-28 09:03:20 +0200

    NonEmptyList

---
+++ src/NonEmptyList.elm
@@ -0,0 +1,13 @@
+module NonEmptyList exposing (..)
+
+import List exposing (..)
+
+type alias NonEmptyList a =
+  { head : a
+  , rest : List a
+  }
+
+asList : (NonEmptyList a) -> List a
+asList nonEmpty =
+  nonEmpty.head :: nonEmpty.rest
+
