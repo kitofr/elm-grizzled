f4ef7ffa55527c3d95b361521068260886b2f9ab
Kristoffer Roupé <kitofr@gmail.com>
2016-11-17 22:37:19 +0100

    Extracted NonEmptyListTests
    - ignored elm.js

---
+++ tests/NonEmptyListTests.elm
@@ -0,0 +1,38 @@
+module NonEmptyListTests exposing (nonEmptyListTests)
+
+import Test exposing (..)
+import List exposing (..)
+import Expect
+import NonEmptyList as NEL exposing (..)
+
+
+nonEmptyListTests =
+    describe "Non empty list"
+        [ test "has at least one item" <|
+            \() ->
+                NonEmptyList "a" []
+                    |> NEL.head
+                    |> Expect.equal "a"
+        , test "asList creates a list" <|
+            \() ->
+                NonEmptyList "a" [ "b" ]
+                    |> NEL.asList
+                    |> Expect.equal [ "a", "b" ]
+        , test "count" <|
+            \() ->
+                NonEmptyList "a" [ "b", "c" ]
+                    |> NEL.count
+                    |> Expect.equal 3
+        , test "add" <|
+            \() ->
+                NonEmptyList "a" [ "b" ]
+                    |> NEL.add "c"
+                    |> NEL.asList
+                    |> Expect.equal [ "a", "b", "c" ]
+        , test "merge" <|
+            \() ->
+                NonEmptyList "a" [ "b" ]
+                    |> NEL.merge (NonEmptyList "c" [ "d" ])
+                    |> NEL.asList
+                    |> Expect.equal [ "a", "b", "c", "d" ]
+        ]
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,6 +1,7 @@
 module Tests exposing (..)
 
 import UtilTests exposing (utilTests)
+import NonEmptyListTests exposing (nonEmptyListTests)
 import Test exposing (..)
 import List exposing (..)
 import Expect
@@ -8,38 +9,6 @@ import String
 import Types exposing (..)
 import NonEmptyList as NEL exposing (..)
 import Util exposing (cycleBy)
-
-
-nonEmptyListTests =
-    describe "Non empty list"
-        [ test "has at least one item" <|
-            \() ->
-                NonEmptyList "a" []
-                    |> NEL.head
-                    |> Expect.equal "a"
-        , test "asList creates a list" <|
-            \() ->
-                NonEmptyList "a" [ "b" ]
-                    |> NEL.asList
-                    |> Expect.equal [ "a", "b" ]
-        , test "count" <|
-            \() ->
-                NonEmptyList "a" [ "b", "c" ]
-                    |> NEL.count
-                    |> Expect.equal 3
-        , test "add" <|
-            \() ->
-                NonEmptyList "a" [ "b" ]
-                    |> NEL.add "c"
-                    |> NEL.asList
-                    |> Expect.equal [ "a", "b", "c" ]
-        , test "merge" <|
-            \() ->
-                NonEmptyList "a" [ "b" ]
-                    |> NEL.merge (NonEmptyList "c" [ "d" ])
-                    |> NEL.asList
-                    |> Expect.equal [ "a", "b", "c", "d" ]
-        ]
 
 
 preparation : Game -> MissionIntensity -> Game
